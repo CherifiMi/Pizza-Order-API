560d7055110ed84af8c5b386a85a38c6bfd8ff53
CherifiMi <cherifi154949@gmail.com>
2022-06-21 00:53:36 +0100

    pizza data class

---
+++ src/main/kotlin/com/example/model/Pizza.kt
@@ -0,0 +1,15 @@
+package com.example.model
+
+import org.bson.codecs.pojo.annotations.BsonId
+import org.bson.types.ObjectId
+
+data class Pizza(
+    @BsonId
+    val id: String= ObjectId().toString(),
+    val name: String,
+    val prices: String,
+    val dec: String,
+    val rating: Float,
+    val pic_url: String,
+    val ing: String,
+)
